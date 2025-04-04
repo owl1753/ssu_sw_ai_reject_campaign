51337da22bb38847b461950d40180f06d9d5d560
owl1753 <bighammer1753@gmail.com>
2025-04-05 05:33:37 +0900

    fix: 안쓰는 변수 제거

---
+++ fiebase/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmeS3SclslZ-6G6y8L9ZfHgwreVTVnm3k",
+  authDomain: "swairejectcampaign.firebaseapp.com",
+  databaseURL: "https://swairejectcampaign-default-rtdb.firebaseio.com",
+  projectId: "swairejectcampaign",
+  storageBucket: "swairejectcampaign.firebasestorage.app",
+  messagingSenderId: "481103560591",
+  appId: "1:481103560591:web:da5fb44f45f87eb82cd5fa",
+  measurementId: "G-VHSEF9SGPC"
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
