815d5d32854e6d8b23e294bd3fba526c67b5bb67
owl1753 <bighammer1753@gmail.com>
2025-04-05 00:51:33 +0900

    feat: 메인 디자인 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-          <h1>반대 서명 운동</h1>
-          <Button asChild size="lg" className="w-full" variant="outline">
-              <Link href="/signature-form">
-                  서명하기
-              </Link>
-          </Button>
-      </main>
-    </div>
-  );
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full mx-auto max-w-3xl">
+                <h1>반대 서명 운동</h1>
+                <Button asChild size="lg" className="w-full" variant="outline">
+                    <Link href="/signature-form">
+                        서명하기
+                    </Link>
+                </Button>
+            </main>
+        </div>
+    );
 }
