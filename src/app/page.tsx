1272c3562e2fdaff1b5009b9f3bf9be2e77d9c64
owl1753 <bighammer1753@gmail.com>
2025-04-05 03:52:42 +0900

    feat: 메인 화면 디자인 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
             <main className="flex flex-col gap-[32px] row-start-2 items-center mx-auto max-w-3xl">
                 <section>
                     <h1 className="font-bold text-lg mb-4">
-                        소프트웨어학부・AI융합학부 통폐합, <br />
-                        학생이 외면당한 결정에 반대합니다. <br />
+                        소프트웨어학부・AI융합학부 통폐합, <br/>
+                        학생이 외면당한 결정에 반대합니다. <br/>
                         반대 온라인 서명운동에 동참해 주세요!
                     </h1>
                     <Card className="mb-4">
@@ -21,23 +21,25 @@ export default function Home() {
                             <b className="text-red-500">800명</b> 참여중!
                         </CardContent>
                     </Card>
-                    <h2 className="text-base mb-1.5">
-                        통폐합은 학교에서 제시한 문제를 해결할 수 없음
+                    <h2 className="text-base mb-1.5 font-semibold">
+                        학생 이익·학습권을 침해하는 도입안입니다.
                     </h2>
-                    <ol className="text-sm mb-4">
-                        <ul>자유 전공학부 정원 쏠림 대책 부재</ul>
-                        <ul>교수·강의실 부족 문제 해결 불가</ul>
-                        <ul>AI혁신대학 15억 지원금 활용 계획 불명확 및 선정 불확실성</ul>
-                    </ol>
-                    <h2 className="text-base mb-1.5">
-                        학생 이익·학습권이 보장되지 않음
+                    <p className="text-sm mb-4">
+                        커리큘럼이 다른 각 학부의 정체성과 학습 내용을 훼손합니다. 학생들이 학과를 선택할 때 기대한 학습 내용을 보장받을 수 없습니다.
+                        <li> 실제로 스마트시스템소프트웨어에서 AI융합학과로 전환된 후 특정 과목이 폐지된 사례가 있습니다.</li>
+                    </p>
+                    <h2 className="text-base mb-1.5 font-semibold">
+                        15억 지원이 확정적이지 않으며, 현재의 문제 해결을 위한 구체적인 활용안이 없습니다.
                     </h2>
-                    <ol className="text-sm">
-                        <ul>각 학부 정체성 훼손</ul>
-                        <ul>소프트웨어학부/AI융합학부 중점 분야 차이 무시</ul>
-                        <ul>5년내 졸업 어려운 학생들 학부명 보장 문제</ul>
-                        <ul>과거 스마트시스템소프트웨어 졸업생 학부명 보장 약속 불이행</ul>
-                    </ol>
+                    <p className="text-sm mb-4">
+                        이렇게 도입안이 시행된다면, 15억이 지원되더라도 교수·강의실 부족과 자유전공학부 쏠림 문제를 해결할 수 없습니다.
+                    </p>
+                    <h2 className="text-base mb-1.5 font-semibold">
+                        휴학생과 재학생에 대한 보호안이 없습니다.
+                    </h2>
+                    <p className="text-sm">
+                        5년간 기존 학부명 졸업장 보장에 대한 법적 효력이 있는 문서가 존재하지 않고, 보장되더라도 군휴학을 하는 경우 5년 내에 졸업이 불가합니다.
+                    </p>
                 </section>
                 <Button asChild size="lg" className="w-full" variant="outline">
                     <Link href="/signature-form">
