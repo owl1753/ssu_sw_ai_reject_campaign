fadf34e66bc07d96d31610bfbce863b6d7928760
nebulaBdj <dongdung5070@gmail.com>
2025-04-05 07:46:10 +0900

    feat: 데이터 페칭을 위한 dataURLtoFile 함수 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,91 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
-    const [signatureCount, setSignatureCount] = useState<number | null>(null);
+  const [signatureCount, setSignatureCount] = useState<number | null>(null);
 
-    useEffect(() => {
-        // EventSource를 사용하여 SSE 연결
-        const eventSource = new EventSource('signature/size');
+  useEffect(() => {
+    // EventSource를 사용하여 SSE 연결
+    const eventSource = new EventSource("signature/size");
 
-        // 데이터를 수신하면 상태 업데이트
-        eventSource.onmessage = (event) => {
-            const {size} = JSON.parse(event.data);
-            setSignatureCount(size);
-        };
+    // 데이터를 수신하면 상태 업데이트
+    eventSource.onmessage = (event) => {
+      const { size } = JSON.parse(event.data);
+      setSignatureCount(size);
+    };
 
-        // 오류 처리
-        eventSource.onerror = (error) => {
-            console.error('EventSource error:', error);
-            eventSource.close();
-        };
+    // 오류 처리
+    eventSource.onerror = (error) => {
+      console.error("EventSource error:", error);
+      eventSource.close();
+    };
 
-        // 컴포넌트 언마운트 시 연결 닫기
-        return () => {
-            eventSource.close();
-        };
-    }, []);
+    // 컴포넌트 언마운트 시 연결 닫기
+    return () => {
+      eventSource.close();
+    };
+  }, []);
 
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-4 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center mx-auto max-w-3xl">
-                <section>
-                    <h1 className="font-bold text-lg mb-4">
-                        소프트웨어학부・AI융합학부 통폐합, <br />
-                        학생이 외면당한 결정에 반대합니다. <br />
-                        반대 온라인 서명운동에 동참해 주세요!
-                    </h1>
-                    <Card className="mb-4">
-                        <CardHeader>
-                            <CardTitle className="text-nowrap">반대 서명운동 실시간현황</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            {signatureCount !== null ? (
-                                <b className="text-red-500">{signatureCount}명</b>
-                            ) : (
-                                <span className="text-sm text-gray-500">로딩 중...</span>
-                            )}{" "}
-                            참여중!
-                        </CardContent>
-                    </Card>
-                    <h2 className="text-base mb-1.5 font-semibold">
-                        학생 이익·학습권을 침해하는 도입안입니다.
-                    </h2>
-                    <p className="text-sm mb-4">
-                        커리큘럼이 다른 각 학부의 정체성과 학습 내용을 훼손합니다. 학생들이 학과를 선택할 때 기대한 학습 내용을 보장받을 수 없습니다.
-                        실제로 스마트시스템소프트웨어에서 AI융합학과로 전환된 후 특정 과목이 폐지된 사례가 있습니다.
-                    </p>
-                    <h2 className="text-base mb-1.5 font-semibold">
-                        15억 지원이 확정적이지 않으며, 현재의 문제 해결을 위한 구체적인 활용안이 없습니다.
-                    </h2>
-                    <p className="text-sm mb-4">
-                        이렇게 도입안이 시행된다면, 15억이 지원되더라도 교수·강의실 부족과 자유전공학부 쏠림 문제를 해결할 수 없습니다.
-                    </p>
-                    <h2 className="text-base mb-1.5 font-semibold">
-                        휴학생과 재학생에 대한 보호안이 없습니다.
-                    </h2>
-                    <p className="text-sm">
-                        5년간 기존 학부명 졸업장 보장에 대한 법적 효력이 있는 문서가 존재하지 않고, 보장되더라도 군휴학을 하는 경우 5년 내에 졸업이 불가합니다.
-                    </p>
-                </section>
-                <Button asChild size="lg" className="w-full" variant="outline">
-                    <Link href="/signature-form">서명하기</Link>
-                </Button>
-            </main>
-        </div>
-    )
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-4 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center mx-auto max-w-3xl">
+        <section>
+          <h1 className="font-bold text-lg mb-4">
+            소프트웨어학부・AI융합학부 통폐합, <br />
+            학생이 외면당한 결정에 반대합니다. <br />
+            반대 온라인 서명운동에 동참해 주세요!
+          </h1>
+          <img src="http://localhost:3000/179f5e10-ad27-46dc-bf0e-311714aa38e1" />
+          <Card className="mb-4">
+            <CardHeader>
+              <CardTitle className="text-nowrap">
+                반대 서명운동 실시간현황
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              {signatureCount !== null ? (
+                <b className="text-red-500">{signatureCount}명</b>
+              ) : (
+                <span className="text-sm text-gray-500">로딩 중...</span>
+              )}{" "}
+              참여중!
+            </CardContent>
+          </Card>
+          <h2 className="text-base mb-1.5 font-semibold">
+            학생 이익·학습권을 침해하는 도입안입니다.
+          </h2>
+          <p className="text-sm mb-4">
+            커리큘럼이 다른 각 학부의 정체성과 학습 내용을 훼손합니다. 학생들이
+            학과를 선택할 때 기대한 학습 내용을 보장받을 수 없습니다. 실제로
+            스마트시스템소프트웨어에서 AI융합학과로 전환된 후 특정 과목이 폐지된
+            사례가 있습니다.
+          </p>
+          <h2 className="text-base mb-1.5 font-semibold">
+            15억 지원이 확정적이지 않으며, 현재의 문제 해결을 위한 구체적인
+            활용안이 없습니다.
+          </h2>
+          <p className="text-sm mb-4">
+            이렇게 도입안이 시행된다면, 15억이 지원되더라도 교수·강의실 부족과
+            자유전공학부 쏠림 문제를 해결할 수 없습니다.
+          </p>
+          <h2 className="text-base mb-1.5 font-semibold">
+            휴학생과 재학생에 대한 보호안이 없습니다.
+          </h2>
+          <p className="text-sm">
+            5년간 기존 학부명 졸업장 보장에 대한 법적 효력이 있는 문서가
+            존재하지 않고, 보장되더라도 군휴학을 하는 경우 5년 내에 졸업이
+            불가합니다.
+          </p>
+        </section>
+        <Button asChild size="lg" className="w-full" variant="outline">
+          <Link href="/signature-form">서명하기</Link>
+        </Button>
+      </main>
+    </div>
+  );
 }
--- src/app/signature-form/page.tsx
+++ src/app/signature-form/page.tsx
@@ -1,172 +1,213 @@
-"use client"
+"use client";
 
-import {Button} from "@/components/ui/button";
-import {zodResolver} from "@hookform/resolvers/zod"
-import {useForm} from "react-hook-form"
-import {z} from "zod"
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormSignaturePad} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Checkbox} from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormSignaturePad,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
-    name: z.string().min(2, {
-      message: "이름은 두 글자 이상으로 입력해주세요.",
-    }),
-    studentId: z.string().length(8, {
-      message: "학번을 확인해주세요.",
-    }),
-    department: z.string().min(1, {
-      message: "학부를 확인해주세요.",
-    }),
-    signature: z.string().min(1, {
-      message: "서명을 입력해주세요.",
-    }),
-    termAgree: z.boolean().refine(val => val, {
-        message: "이용약관에 동의해주세요.",
-      }),
-  })
-  
+  name: z.string().min(2, {
+    message: "이름은 두 글자 이상으로 입력해주세요.",
+  }),
+  studentId: z.string().length(8, {
+    message: "학번을 확인해주세요.",
+  }),
+  department: z.string().min(1, {
+    message: "학부를 확인해주세요.",
+  }),
+  signature: z.string().min(1, {
+    message: "서명을 입력해주세요.",
+  }),
+  termAgree: z.boolean().refine((val) => val, {
+    message: "이용약관에 동의해주세요.",
+  }),
+});
 
 export default function SignatureForm() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-            studentId: "",
-            department: "",
-            termAgree: false,
-        },
+  const { push } = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      studentId: "",
+      department: "",
+      termAgree: false,
+    },
+  });
+
+  function dataURLtoFile(dataurl: string, filename: string): File {
+    const arr = dataurl.split(",");
+    const mimeMatch = arr[0].match(/:(.*?);/);
+    if (!mimeMatch) throw new Error("Invalid data URL");
+    const mime = mimeMatch[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  }
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { name, studentId, department, signature } = values;
+
+    console.log("서명 base64", signature);
+
+    const signatureFile = dataURLtoFile(
+      signature,
+      `${name}-${studentId}-${department}-signature.png`
+    );
+
+    console.log("서명 파일", signatureFile);
+
+    const blobUrl = URL.createObjectURL(signatureFile);
+
+    console.log("서명 파일", blobUrl);
+
+    const response = await fetch("/signature", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        studentId,
+        department,
+      }),
     });
 
-    async function onSubmit(values: z.infer<typeof formSchema>) {
-        const {name, studentId, department} = values;
+    const data = await response.json();
 
-        const response = await fetch('/signature', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name,
-                studentId,
-                department,
-            })
-        });
-
-        const data = await response.json();
-
-        if (data.status === 201) {
-            alert("서명이 정상적으로 등록되었습니다!");
-            form.reset();
-        } else if (data.status === 400) {
-            alert("오류가 발생했습니다. 다시 시도해주세요.");
-        }
+    if (data.status === 201) {
+      alert("서명이 정상적으로 등록되었습니다!");
+      form.reset();
+      push("/");
+    } else if (data.status === 400) {
+      alert("오류가 발생했습니다. 다시 시도해주세요.");
     }
+  }
 
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto w-full max-w-3xl space-y-8">
-                {/* 이름 */}
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>이름</FormLabel>
-                      <FormControl>
-                        <Input placeholder="홍길동" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-      
-                {/* 학번 */}
-                <FormField
-                  control={form.control}
-                  name="studentId"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>학번</FormLabel>
-                      <FormControl>
-                        <Input placeholder="20000000" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-      
-                {/* 학부 */}
-                <FormField
-                  control={form.control}
-                  name="department"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>학부</FormLabel>
-                      <FormControl>
-                        <Input placeholder="예: 소프트웨어학부" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-      
-                {/* ✅ 시그니처 패드 추가 */}
-                <FormField
-                  control={form.control}
-                  name="signature"
-                  render={() => (
-                    <FormItem>
-                      <FormLabel>서명</FormLabel>
-                      <FormControl>
-                        <FormSignaturePad />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mx-auto w-full max-w-3xl space-y-8"
+          >
+            {/* 이름 */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>이름</FormLabel>
+                  <FormControl>
+                    <Input placeholder="홍길동" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-                {/* 이용약관 */}
-                <FormField
-                  control={form.control}
-                  name="termAgree"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <div className="flex items-center space-x-2">
-                          <Checkbox
-                            id="terms"
-                            checked={field.value}
-                            onCheckedChange={field.onChange}
-                          />
-                          <label
-                            htmlFor="terms"
-                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                          >
-                            이용 약관에 동의합니다.
-                          </label>
-                        </div>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-      
-                {/* 제출 버튼 */}
-                <div className="flex gap-3">
-                  <Button variant="default" className="flex-1" type="submit">제출 하기</Button>
-                  <Button asChild variant="outline" className="flex-1">
-                    <Link href="/">
-                      메인 화면으로
-                    </Link>
-                  </Button>
-                </div>
-              </form>
-            </Form>
-          </main>
-        </div>
-      );
-    }      
+            {/* 학번 */}
+            <FormField
+              control={form.control}
+              name="studentId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>학번</FormLabel>
+                  <FormControl>
+                    <Input placeholder="20000000" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* 학부 */}
+            <FormField
+              control={form.control}
+              name="department"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>학부</FormLabel>
+                  <FormControl>
+                    <Input placeholder="예: 소프트웨어학부" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* ✅ 시그니처 패드 추가 */}
+            <FormField
+              control={form.control}
+              name="signature"
+              render={() => (
+                <FormItem>
+                  <FormLabel>서명</FormLabel>
+                  <FormControl>
+                    <FormSignaturePad />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* 이용약관 */}
+            <FormField
+              control={form.control}
+              name="termAgree"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex items-center space-x-2">
+                      <Checkbox
+                        id="terms"
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                      <label
+                        htmlFor="terms"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        이용 약관에 동의합니다.
+                      </label>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* 제출 버튼 */}
+            <div className="flex gap-3">
+              <Button variant="default" className="flex-1" type="submit">
+                제출 하기
+              </Button>
+              <Button asChild variant="outline" className="flex-1">
+                <Link href="/">메인 화면으로</Link>
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </main>
+    </div>
+  );
+}
