5e38379a425b0fcdc954a21dcc0faaf951dcfeaa
owl1753 <bighammer1753@gmail.com>
2025-04-05 00:46:13 +0900

    feat: 폼 디자인 깔끔하게 수정

--- src/app/signature-form/page.tsx
+++ src/app/signature-form/page.tsx
@@ -40,7 +40,7 @@ export default function SignatureForm() {
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
                 <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="mx-auto w-full max-w-3xl space-y-8">
                         <FormField
                             control={form.control}
                             name="name"
